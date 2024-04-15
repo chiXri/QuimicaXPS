96506ce4046f486671f495b63e655fb544bdfc33
chiXri <marco_chirnos@hotmail.es>
2024-04-15 22:31:58 +0200

    nuevo modelo interaces

---
+++ script.js
@@ -0,0 +1,10 @@
+// Script para el desplazamiento suave al hacer clic en los enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
